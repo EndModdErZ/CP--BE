c53c927979368816c6c5f8cf05d48c903c29da20
EndModdErZ <121460790+EndModdErZ@users.noreply.github.com>
2023-04-26 00:12:01 +0000

    update BP/scripts/Croptopia.js

--- BP/scripts/Croptopia.js
+++ BP/scripts/Croptopia.js
@@ -2,6 +2,7 @@ import { world } from "@minecraft/server";
 
 
 world.events.entityHurt.subscribe(async function(data){
+  const { damagingEntity } = data; if (!damagingEntity) return;
   
 });
 world.events.blockBreak.subscribe
