c231dc36599f06bca0ff8a521fcb5cd1b9b07897
EndModdErZ <121460790+EndModdErZ@users.noreply.github.com>
2023-04-26 01:06:27 +0000

    update BP/scripts/Croptopia.js

--- BP/scripts/Croptopia.js
+++ BP/scripts/Croptopia.js
@@ -1,8 +1,17 @@
 import { world } from "@minecraft/server";
+import { getItem } from "./functions";
 
 
-world.events.entityHurt.subscribe(async function(data){
-  const { damagingEntity } = data; if (!damagingEntity) return;
+world.events.entityHurt.subscribe(async function({hurtEntity,damageSource}){
+  const { damagingEntity } = damageSource; if (!damagingEntity) return;
   
+  switch (getItem(damagingEntity).typeId) {
+    case 'emrz-croptopia:frying_pan':
+     
+      break;
+    
+    default:
+
+  }
 });
 world.events.blockBreak.subscribe
